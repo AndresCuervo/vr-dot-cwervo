07d529409fa5200386fdfa3a75157fa482014d2f
Andres Cuervo <andrescuervor@gmail.com>
2017-04-16 18:38:44 -0400

    Add images, update animation, add so many trees

--- src/site/capstone/index.clj
+++ src/site/capstone/index.clj
@@ -22,82 +22,120 @@
    "https://unpkg.com/aframe-template-component@^3.1.1/dist/aframe-template-component.min.js"
    ])
 
+(def head-el
+  [:head
+   [:title "VR Capstone"]
+   [:meta {:charset "utf-8"}]
+   [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
+   [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+   [:script {:src "https://aframe.io/releases/0.5.0/aframe.min.js"}]
+   (for [src srcs]
+     [:script {:src src}])])
+
+(def a-assets-el
+  [:a-assets
+   ;; [:img {:id "pine-needles-texture" :src "/images/textures/pine-needles.jpg"}]
+   [:img {:id "pine-needles-texture" :src "/images/textures/pine-needles-conic.jpg"}]
+   [:img {:id "bark-cylinder-texture" :src "/images/textures/bark.jpg"}]
+   [:a-mixin {:id "tree-texture" :material "src:#pine-needles-texture"}]
+   [:a-mixin {:id "bark-texture" :material "src:#bark-cylinder-texture"}]
+
+   [:script {:id "treeTemplate"
+             :type "html"}
+    [:a-entity {:id "conicTree-${number}" :position "${pos}"}
+     [:a-cone {:mixin "tree-texture" :radius-bottom "2" :radius-top "0" :height "2"
+               :wireframe "${wireframe}"}
+      [:a-cone {:mixin "tree-texture" :radius-bottom "2" :radius-top "0" :position "0 -1   0"
+                :wireframe "${wireframe}"
+                :animation__wow "property: position;
+                                from: ${pos};
+                                to: 0 -1 ;
+                                dir: alternate;
+                                dur: 2000;
+                                loop: true;"
+                }]
+      [:a-cone {:mixin "tree-texture" :radius-bottom "2" :radius-top "0" :position "0 -1.5 0"
+                :wireframe "${wireframe}"}]
+      [:a-cylinder {:random-rotation "min: 0 0 0; max: 0 360 0" :mixin "bark-texture" :height "2" :radius "0.5" :position "0 -2.5 0"
+                    :wireframe "${wireframe}"}]]]]])
+
 (defn make-box-row [n result]
   ;; Ohh, you can do trees like this, in raw html, maybe?
-(let [size (* 0.1 n)
-      pos (string/join
-                  " "
-                  [(+ -2 n 1) (+ 0.5 (Math/cos n)) -3])]
-  (if (> n 0)
-    (make-box-row (dec n) (list #_[:a-cone {:color "tomato"
-                                            :radius-bottom "2"
-                                            :radius-top size
-                                            :position pos}]
-                                [:a-box
-                                 {:color "#4CC3D9"
-                                  :depth size
-                                  :height size
-                                  :width size
-                                  :rotation "0 45 0"
-                                  :animation__rotate_lol (str
-                                                           "property: rotation;
-                                                           from: 0 45 0;
-                                                           loop: true;
-                                                           easing: linear;
-                                                           to: 0 " (+ 360 (*  45 (Math/cos n))) " 0;")
-                                  :position pos}] result))
-    result)))
+  (let [size (* 0.1 n)
+        pos (string/join
+              " "
+              [(+ -2 n 1) (+ 0.5 (Math/cos n)) -3])
+        y-pos (Math/floor (+ 360 (*  45 (Math/cos n))))]
+    (if (> n 0)
+      (make-box-row (dec n) (list #_[:a-cone {:color "tomato"
+                                              :radius-bottom "2"
+                                              :radius-top size
+                                              :position pos}]
+                                  [:a-box
+                                   {:color "#4CC3D9"
+                                    :depth size
+                                    :height size
+                                    :width size
+                                    :rotation "0 45 0"
+                                    :animation__rotate_lol (str
+                                                             "property: rotation;
+                                                             from: 0 45 0;
+                                                             loop: true;
+                                                             easing: linear;
+                                                             to: 0 " y-pos " 0;")
+                                    :position pos}] result))
+      result)))
 
 (defn render [{global-meta :meta
                entries :entries}]
   (html
-    [:head
-     [:title (:site-title global-meta)]
-     [:meta {:charset "utf-8"}]
-     [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
-     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-     [:script {:src "https://aframe.io/releases/0.5.0/aframe.min.js"}]
-     (for [src srcs]
-       [:script {:src src}])]
+    head-el
     [:body
      [:a-scene
+      a-assets-el
 
-      [:a-assets
-       [:img {:id "pine-needles-texture" :src "/images/textures/pine-needles.jpg"}]
-       [:img {:id "bark-cylinder-texture" :src "/images/textures/bark.jpg"}]
-       [:a-mixin {:id "tree-texture" :material "src:#pine-needles-texture"}]
-       [:a-mixin {:id "bark-texture" :material "src:#bark-cylinder-texture"}]
+      [:a-sky {:src "/images/panoramas/test-arb1.jpg" :rotation "0 -130 0"
+               :animation__color "property: color;
+                                 from: #2EAFAC;
+                                 to: #BBAAEE;
+                                 easing: linear;
+                                 dir: alternate;
+                                 dur: 1500;
+                                 loop: true;"}]
 
-       [:script {:id "treeTemplate"
-                 :type "html"}
-         #_"<a-entity id=\"conicTree-${number}\" position=\"${pos}\">
-                <a-cone mixin=\"tree-texture\" radius-bottom=\"2\" radius-top=\"0\" height=\"2\">
-                    <a-cone mixin=\"tree-texture\" radius-bottom=\"2\" radius-top=\"0\" position=\"0 -1   0\"></a-cone>
-                    <a-cone mixin=\"tree-texture\" radius-bottom=\"2\" radius-top=\"0\" position=\"0 -1.5 0\"></a-cone>
-                    <!-- <a-cylinder mixin=\"bark-color\" height=\"1\" radius=\"0.5\" position=\"0 -2.5 0\"     ></a-cylinder> -->
-                    <a-cylinder random-rotation=\"min: 0 0 0; max: 0 360 0\" mixin=\"bark-texture\" height=\"2\" radius=\"0.5\" position=\"0 -2.5 0\"     ></a-cylinder>
-                </a-cone>
-            </a-entity>"
-         [:a-entity {:id "conicTree-${number}" :position "${pos}"}
-          [:a-cone {:mixin "tree-texture" :radius-bottom "2" :radius-top "0" :height "2"}
-           [:a-cone {:mixin "tree-texture" :radius-bottom "2" :radius-top "0" :position "0 -1   0"}]
-           [:a-cone {:mixin "tree-texture" :radius-bottom "2" :radius-top "0" :position "0 -1.5 0"}]
-           [:a-cylinder {:random-rotation "min: 0 0 0; max: 0 360 0" :mixin "bark-texture" :height "2" :radius "0.5" :position "0 -2.5 0"}]]]]]
-
-
-      [:a-sky {:src "/images/panoramas/manic-night/archway.jpg" :rotation "0 -130 0"}]
-
-      [:a-entity {:template "src: #treeTemplate" :data-number "1" :data-pos "-2 3 -4"}]
-      [:a-entity {:template "src: #treeTemplate" :data-number "2" :data-pos "1 3 -4"}]
-      [:a-entity {:template "src: #treeTemplate" :data-number "3" :data-pos "0 3 -4"}]
-
-      (for [n (range 4 96)]
-        [:a-entity {:rand-data-pos "" :template "src: #treeTemplate" :data-number n}])
+      (for [n (range 1 96)]
+        [:a-entity {:rand-data-pos "" :template "src: #treeTemplate"
+                    :data-number n
+                    :data-wireframe "false"
+                    }])
 
       ;; Funny expanding cone
-      #_[:a-cone {:color "#2EAFAC"
-                :position "-2 0 -4"
+      (for [n (range -50 150 20)]
+        [:a-cone {:color "#2EAFAC"
+                  :wireframe "true"
+                  :position (string/join " "
+                                         [(/ n 5)
+                                          (Math/cos n)
+                                          -3])
+                  :radius-bottom "2" :radius-top "0"
+                  :animation__pointy "startEvents: mouseenter;
+                                     pauseEvents: mouseleave;
+                                     property: radius-top;
+                                     loop: true;
+                                     easing: linear;
+                                     from: 0; to: 3;
+                                     dir: alternate;
+                                     dur: 1000;"
+                  :data-radius-bottom "2"}])
 
+      (make-box-row 20 '())
+
+      #_(for [n (range 0 600 60)]
+        [:a-cone {:color "#2EAFAC"
+                :position (string/join " "
+                                       [(Math/sin n)
+                                        (Math/cos n)
+                                        -3])
                 :radius-bottom "2" :radius-top "0"
                 :animation__pointy "property: radius-top;
                                    loop: true;
@@ -105,9 +143,7 @@
                                    from: 0; to: 3;
                                    dir: alternate;
                                    dur: 1000;"
-                :data-radius-bottom "2"}]
-
-      #_(make-box-row 20 '())
+                :data-radius-bottom "2"}])
 
       ;; Sun???
       (for [rotation ["25 0 10" "25 15 15"]]
@@ -124,7 +160,7 @@
                            dur: 3000;"
           }])
 
-      (let [wireframe false]
+      (let [wireframe true]
         [:a-entity#egg-whites
          {:geometry "primitive: circle; radius: 1;"
           :material (str "color: white;"
