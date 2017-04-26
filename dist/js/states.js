bfcc67fabb8b16a39f75714eab4dbb739f0d31b8
Andres Cuervo <andrescuervor@gmail.com>
2017-04-26 12:51:06 -0400

    Deploy commit Wed Apr 26 12:51:06 EDT 2017

--- dist/js/states.js
+++ dist/js/states.js
@@ -1,4 +1,4 @@
-AFRAME.registerComponent('fuse-trigger', {
+AFRAME.registerComponent('fsm-event-trigger', {
     schema: {
         event: {type: 'string'}
     },
@@ -16,6 +16,30 @@ AFRAME.registerComponent('fuse-trigger', {
         });
     }
 });
+
+
+/*
+ * ---- Description of states -----
+ * Scenes:
+ *    - initial: "black"
+ *    - removetitle | "black "-> "green"
+ *      - Event: Removes the starting sphere and title cards from the scene
+ *      - "green" : Trees are gently rotating, in their circles, and after 5 seconds
+ *        a sphere appears on one of the trees, text pops up all around you saying you
+ *        have to stare at the spheres until they go away (you have to find and stare at
+ *        3 spheres moving alongside trees) (the spheres have numbers, that always face
+ *        you using the look-at component!)
+ *    - worry | "green"-> "yellow"
+ *      - Event: animates the trees slowly coming to a freeze, then all turning yellow and red
+ *        and slowly sinking into the ground, meanwhile as these set animations finish (~10s)
+ *        these black sad trees rise, audio of a bird chirping comes into the scene, and the
+ *        a charcoal gray floor rises into the scene. wow
+ *      - "yellow" : The charcoal trees (there are only 4 of them), come up to the player (who's
+ *        still at [0 1.6 0]) and you have to follow dots up and down each tree until they "pop" back to
+ *        life (original clone-like tree lol)
+ *    - ending | "yellow"-> "gray"
+ *      - Event: the trees, once they're all actually
+*/
 
 function fadeOutElements(selectorString, time) {
     for (el of document.querySelectorAll(selectorString)) {
@@ -54,7 +78,7 @@ var fsm = StateMachine.create({
             fadeOutElements('.title_plane_text', 0);
             fadeOutElements('.title_plane', 10);
             fadeOutElements('.title_text', 20);
-            fadeOutElements('#black_mask', 30);
+            fadeOutElements('#intro_mask', 30);
         },
         onpanic:  function(event, from, to, msg) { alert('panic! ' + msg);               },
         onclear:  function(event, from, to, msg) { alert('thanks to ' + msg);            },
@@ -66,3 +90,48 @@ var fsm = StateMachine.create({
 
 
 console.log("FSM ran");
+
+/* --- saving a version of this for later idk ---
+AFRAME.registerComponent('fsm-event-trigger', {
+    schema: {
+        event: {type: 'string'},
+        wait: {default: 0},
+        timeClicked: {default: 0},
+        staring : {default: false}
+    },
+
+    init: function () {
+        var currentPosition = this.el.object3D.position;
+        console.log("adding click handler ...");
+        var fsmEvent = this.data.event;
+        var waitTime = this.data.wait;
+        var timeClicked = this.data.timeClicked;
+        var staring = this.data.staring;
+        console.log("waiting for " + this.el + "to be pressed " + waitTime + "seconds");
+        this.el.addEventListener('click', function (evt) {
+            // var randomIndex = Math.floor(Math.random() * COLORS.length);
+            // var newColor = COLORS[randomIndex];
+            // this.setAttribute('material', 'color', newColor);
+            if (waitTime >= timeClicked) {
+                fsm[fsmEvent]();
+                console.log('Fuse: I was clicked at: ', evt.detail.intersection.point);
+            }
+        });
+
+        this.el.addEventListener('mouseenter', function (evt) {
+            staring = true;
+        });
+        this.el.addEventListener('mouseleave', function (evt) {
+            staring = false;
+            timeClicked = 0;
+        });
+    },
+
+    // tick: function () {
+        // if (this.data.staring) {
+        //     this.data.timeClicked++;
+        // }
+        // console.log("this.data.timeClicked: " + this.data.timeClicked);
+    // }
+});
+ * */
