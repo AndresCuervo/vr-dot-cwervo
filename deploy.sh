065eb5ad9a65d12ad805f3ad70f8f37184f872f4
Andres Cuervo <andrescuervor@gmail.com>
2017-05-11 13:28:54 -0400

    Update fun little message for deploy script

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,13 @@ fi
 
 boot build
 cp -rf target/public/ "$out"
-cp -rf resources/public/assets/"$out"
+cp -rf resources/public/assets/ "$out"
 cp -rf resources/public/images "$out"
 cp -rf resources/public/js "$out"
 rm -f "$out"/CNAME # Remove old CNAME, -f to quiet the warnings
 echo "vr.cwervo.com" > "$out"/CNAME
 git add "$out"
+echo "🎉🎉✨🎉✨🎉✨ ----- Hi! You gotta gimme your password to commit to push :) ----- 🎉✨🎉✨🎉✨🎉✨"
 git commit -S -m "Deploy commit $(date)"
 # Oh, need a new commit before subtree command to get it to sync!
 # git subtree push --prefix "$out" origin gh-pages
